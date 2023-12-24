14c59444c1a0476ecdbc1b6c492fb2f004e13431
Marymota <dg.marciamota@gmail.com>
2023-12-24 13:17:22 +0000

    chat implementation with interfaces

---
+++ frontend/src/service/apiService.tsx
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+const BACK_PORT = process.env.BACK_PORT;
+
+export const fetchMessage = async () => {
+  try {
+    const response = await axios.get('${BACK_PORT}/messages');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching messages', error);
+    throw error;
+   }
+};
+
+export const fetchUsers = async () => {
+  try {
+    const response = await axios.get(`${BACK_PORT}/users`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching users', error);
+    throw error;
+  }
+};
+  
--- shared/interfaces/talk.interface.ts
+++ shared/interfaces/talk.interface.ts
@@ -1,18 +1,28 @@
 export interface User {
-    id: string;
-    userName: string;
+    id: string
+    userName: string
+    socketId: string
+}
+
+export interface Room {
+    name: string
+    admin: User
+    users: User[]
+    // Probably will need to create an array of admins
 }
 
 export interface Message {
-    sender: User;
-    date: string;
-    content: string;
+    sender: User
+    date: string
+    content: string
+    channelName: string
 }
 
 export interface ServerToClientEvents {
-    talk: (e: Message) => void;
+    talk: (e: Message) => void
 }
 
 export interface ClientToServerEvents {
-    talk:(e: Message) => void;
+    talk: (e: Message) => void
+    joinChannel: (e: { user: User; channelName: string }) => void
 }
